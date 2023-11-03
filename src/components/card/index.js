d295428814d5497ab4c7ca20e236b0423c81d443
aditakbars <aditakbars33@gmail.com>
2023-11-03 15:18:56 +0700

    adding desc

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -5,6 +5,7 @@ export default function Card({ data, onClick }) {
     // Tambahkan pengecekan untuk data, data.i, dan data.i.imageUrl
     if (!data || !data.i || !data.i.imageUrl) {
         const imageUrl = `https://source.unsplash.com/200x300?${data.l}`;
+        const desc = "gx ad gambarny";
         
         return (
             <div className="card" onClick={onClick}>
@@ -13,7 +14,7 @@ export default function Card({ data, onClick }) {
                 </figure>
                 <div className="card-info">
                     <h3>{data.l}</h3>
-                    <p>{data.q}</p>
+                    <p>{desc}</p>
                 </div>
             </div>
         );
