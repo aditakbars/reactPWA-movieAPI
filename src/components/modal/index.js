e4c9a78f5c9ac8300c1186eef23c567e96539965
aditakbars <aditakbars33@gmail.com>
2023-11-03 13:10:36 +0700

    fixing the modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./index.css";
 
+export default function Modal({ isShow, data, onCancel }) {
+    if (!data || !data.i || !data.i.imageUrl) {
+        const imageUrl = `https://source.unsplash.com/720x720?${data && data.l}`;
+        return (
+            <div className={!isShow ? "hidden" : ""} datacy="modal-delete">
+                <div className="modal-bg" onClick={onCancel}></div>
+                <div className="modal">
+                    {data && <img src={imageUrl} alt={data.l} />}
+                </div>
+            </div>
+        );
+    }
 
-export default function index({ isShow, data, onCancel }){
     return (
         <div className={!isShow ? "hidden" : ""} datacy="modal-delete">
-            <div className="modal-bg" onClick={onCancel}></div><div className="modal">
-                {data && <img src={data.i.imageUrl} alt="sesuatu"/>}
+            <div className="modal-bg" onClick={onCancel}></div>
+            <div className="modal">
+                {data && <img src={data.i.imageUrl} alt={data.l} />}
             </div>
         </div>
     );
